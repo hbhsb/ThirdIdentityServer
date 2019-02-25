fe38b2c07b55f5299d413e7363ed9e450d2c4bd4
hshua <qnmlgbhuang@gmail.com>
2019-02-25 19:59:23 +0800

    添加项目文件。

---
+++ Config.cs
@@ -0,0 +1,205 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace QuickstartIdentityServer
+{
+    public class Config
+    {
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource("roles","角色", new List<string>{"role"}),
+                new IdentityResource("nationality","国籍", new List<string>(){"nationality"})
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("https://quickstarts/api", "My API",
+                    new List<string>()
+                    {
+                        "role",
+                        "given_name",
+                        "gender",
+                        "nationality"
+                    })
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+
+                // resource owner password grant client
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+                // OpenID Connect implicit flow client (MVC)
+                new Client
+                {
+                    ClientId = "ZnwqE8j-H6kmHeQBM3NH2WbdikUjPrNV",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets =
+                    {
+                        new Secret("jecyL0PrTIxjNf4GUbz0oa_ssRLiJBG8OXfIMzLDjGCEoTV48HHqvK2pasPodPyN".Sha256())
+                    },
+
+                    RedirectUris = { "http://192.168.0.174:3000/callback" },
+                    PostLogoutRedirectUris = { "http://localhost:3000/" },
+
+                    AllowedScopes =
+                    {
+                        "https://quickstarts/api",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                    }
+                },
+                new Client
+                {
+                    ClientId = "SYGtG8b4HcWBICHxkw63173ohARZoaO8",
+                    ClientName = "MVC Client2",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets =
+                    {
+                        new Secret("BPptSgcAhYxUYIIPSbr5SDHG4-Gq8TrP2qsVc44j4YmNqmm-nuc2Ld3heyJQoMmB".Sha256())
+                    },
+
+                    RedirectUris = { "http://localhost:3000/callback" },
+                    PostLogoutRedirectUris = { "http://localhost:3000/" },
+
+                    AllowedScopes =
+                    {
+                        "https://quickstarts/api",
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        "nationality"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "123",
+                    ClientName = "456",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets =
+                    {
+                        new Secret("456".Sha256())
+                    },
+
+                    RedirectUris = { "http://localhost:5000/callback" },
+                    PostLogoutRedirectUris = { "http://localhost:5000/" },
+
+                    AllowedScopes =
+                    {
+                        "https://quickstarts/api",
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        "nationality"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+
+                    RedirectUris =           { "http://localhost:3000/callback.html" },
+                    PostLogoutRedirectUris = { "http://localhost:3000/index.html" },
+                    AllowedCorsOrigins =     { "http://localhost:3000" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "https://quickstarts/api"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "mvc2",
+                    ClientName = "MVC Client2",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+
+                    RedirectUris = { "http://localhost:5004/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5004/signout-callback-oidc" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "https://alice.com")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "3",
+                    Username = "bob",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("ID","123"),
+                        new Claim("name", "Bob"),
+                        new Claim("website", "https://bob.com"),
+                        new Claim("综上所述","fuck"),
+                        new Claim("nationality","美国")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.IdentityModel.Tokens.Jwt;
+using IdentityServer4;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace QuickstartIdentityServer
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddMvc();
+
+
+            //services.AddAuthentication()
+            //    .AddMicrosoftAccount(MicrosoftAccountDefaults.AuthenticationScheme, options =>
+            //    {
+            //        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            //        options.ClientId = "33f1f15d-93d5-4749-9b0e-24fc7c0bf56e";
+            //        options.ClientSecret = "wttGKYI05[vppzBAG913#?_";
+            //    });
+            // configure identity server with in-memory stores, keys, clients and scopes
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseIdentityServer();
+            
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
